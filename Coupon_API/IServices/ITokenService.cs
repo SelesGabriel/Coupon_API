8e7fd5b395a380ce05c82e568280ad2f0afdc7b7
Gabriel Seles <selesgabriel@gmail.com>
2022-04-13 22:18:14 -0300

    Including JWT authorization

---
+++ Coupon_API/IServices/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using Coupon_API.Models;
+
+namespace Coupon_API.IServices;
+
+public interface ITokenService
+{
+    string GerarToken(string key, string issuer, string audience, UserLogin user);
+}
--- Coupon_API/Program.cs
+++ Coupon_API/Program.cs
@@ -1,12 +1,62 @@
 using Coupon_API.Data;
+using Coupon_API.IServices;
 using Coupon_API.Models;
+using Coupon_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogoApi", Version = "v1" });
+    s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space]. Example: \'Bearer 12345abcdef'\",
+    });
+    s.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddDbContext<AppDbContext>();
+
+//add token authentication
+builder.Services.AddSingleton<ITokenService>(new TokenService());
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddAuthorization();
 
 
 builder.Services.AddCors(options => options
@@ -34,7 +84,7 @@ CouponService couponService = new();
 app.MapGet("item", (string idItem) =>
 {
     return Results.Ok(itemService.GetItem(idItem));
-}).WithTags("Item");
+}).WithTags("Item").RequireAuthorization();
 #endregion
 
 #region User
@@ -43,7 +93,7 @@ app.MapGet("user", (AppDbContext context) =>
     return Results.Ok(userService.GetUser(context));
 }).WithTags("User");
 
-app.MapGet("user/{id}", (AppDbContext context,int id) =>
+app.MapGet("user/{id}", (AppDbContext context, int id) =>
 {
     return Results.Ok(userService.GetUser(context, id));
 }).WithTags("User");
@@ -80,4 +130,22 @@ app.MapPost("coupon", (AppDbContext context, Coupon coupon, int idUser) =>
 }).WithTags("Coupon");
 #endregion
 
+#region Login
+app.MapPost("login", [AllowAnonymous] (UserLogin userLogin, ITokenService tokenService) =>
+ {
+     if (userLogin == null)
+         return Results.BadRequest("Invalid Login");
+     if (userLogin.UserName == "admin" && userLogin.Password == "admin")
+     {
+         var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+             app.Configuration["Jwt:Issuer"],
+             app.Configuration["Jwt:Audience"], userLogin);
+         return Results.Ok(new { token = tokenString });
+     }
+     return Results.BadRequest("Invalid Login");
+ }).WithTags("Authentication");
+#endregion
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
